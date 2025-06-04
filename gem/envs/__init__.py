ffae58965821d1a7a543090745c1e84a17a4c325
Zichen <liuzc@sea.com>
2025-06-04 17:51:40 +0800

    refine naming

--- gem/envs/__init__.py
+++ gem/envs/__init__.py
@@ -1,7 +1,7 @@
 from gem.envs.registration import register
 
 register(
-    "GuessTheNumber-v0",
+    "ta:GuessTheNumber-v0",
     "gem.envs.textarena.guess_the_number:GuessTheNumberEnv",
     min_number=1,
     max_number=20,
--- tests/test_env/test_guess_the_number.py
+++ tests/test_env/test_guess_the_number.py
@@ -2,7 +2,7 @@ import gem
 
 
 def test():
-    env = gem.make("GuessTheNumber-v0")
+    env = gem.make("ta:GuessTheNumber-v0")
     obs, _ = env.reset()
 
     done = False
@@ -10,10 +10,30 @@ def test():
         action = env.sample_random_action()
         next_obs, reward, terminated, truncated, _ = env.step(action)
         done = terminated | truncated
-        print("=" * 20)
+        print(
+            "=" * 20,
+            "observation",
+            "=" * 20,
+        )
         print(obs)
+        print(
+            "=" * 20,
+            "action",
+            "=" * 20,
+        )
         print(action)
+        print(
+            "=" * 20,
+            "reward",
+            "=" * 20,
+        )
         print(reward)
+        print(
+            "=" * 20,
+            "next observation",
+            "=" * 20,
+        )
+        print(next_obs)
         obs = next_obs
 
 
