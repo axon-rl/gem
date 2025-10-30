0ae7aa687c25f0ee07628f8a4072fba9a43c6f49
Keyu Duan <104810828@qq.com>
2025-10-30 11:43:57 +0800

    add readme

--- gem/envs/webshop/setup.sh
+++ gem/envs/webshop/setup.sh
@@ -1,9 +1,9 @@
-# echo "Setting up misc webshop environment with conda..."
-# conda install mkl
-# conda install -c conda-forge faiss-cpu
-# conda install -c conda-forge openjdk=11
+echo "Setting up misc webshop environment with conda..."
+conda install mkl
+conda install -c conda-forge faiss-cpu
+conda install -c conda-forge openjdk=11
 
-# python -m spacy download en_core_web_sm
+python -m spacy download en_core_web_sm
 echo "Preprocessing webshop dataset..."
 python gem/envs/webshop/preprocess.py
 echo "Indexing webshop dataset for search engine..."
